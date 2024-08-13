9e58538be707b5bbe82ae3f662618c155cf53ca2
Ethan <ethan.heimer12357@gmail.com>
2024-08-12 21:55:53 -0700

    REF: Refactoring to match CrookedTheme v4.0

--- lua/crookedpastel/pastel.lua
+++ lua/crookedpastel/pastel.lua
@@ -41,6 +41,18 @@ theme.editor.menu = {bg = backagroundTwo}
 theme.editor.title = {fg = colorFive}
 theme.editor.search = {fg = colorFive}
 theme.editor.nontext = {fg = colorFour}
+
+theme.netrw.dir = {fg = colorSeven}
+theme.netrw.exe = {fg = colorFive}
+theme.netrw.symlink = {fg = colorTen}
+theme.netrw.version = {fg = colorFour}
+theme.netrw.gray = {fg = colorOne}
+theme.netrw.date = {fg = colorTen}
+theme.netrw.time = {fg = colorSeven}
+theme.netrw.comment = {fg = colorFive}
+theme.netrw.base = {fg = colorSeven}
+theme.netrw.change = {fg = colorThree}
+theme.netrw.cursorLine = {bg = backagroundTwo}
 --[[
 theme.keywordColor = {fg = colorTen, bold = true}
 theme.stringColor = {fg = colorTwo, italic = true, underline = true}
