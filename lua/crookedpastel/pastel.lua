bdbd2392c65f42e21a4c5016e4fd733d81a49ac5
Ethan <ethan.heimer12357@gmail.com>
2024-08-13 00:39:14 -0700

    REF: Refactoring to match CrookedTheme v4.0

--- lua/crookedpastel/pastel.lua
+++ lua/crookedpastel/pastel.lua
@@ -68,42 +68,4 @@ theme.keyword.define = {fg  = colorThree, italic = true}
 theme.keyword.builtinConstant = {fg = colorSeven, underline = true, bold = true}
 theme.keyword.definitions = {fg = colorThree, bold = true, italic = true}
 
---[[
-theme.keywordColor = {fg = colorTen, bold = true}
-theme.stringColor = {fg = colorTwo, italic = true, underline = true}
-theme.fieldColor = {fg = colorSix, bold = false}
-theme.classColor = {fg = colorTwo, bold = true}
-theme.variableColor = {fg = colorTwo}
-theme.booleanColor = {fg = colorFour, bold = true, underline = true}
-theme.constantColor = {fg = colorSeven, underline = true}
-theme.functionColor = {fg = colorSeven, bold = true}
-theme.builtinFunctionColor = {bold = true, underline = true, italic = true}
-theme.attributeColor = {fg = colorFive}
-theme.conditionalColor = {fg = colorSix}
-theme.moduleColor = {fg = colorSeven, bold = true}
-theme.macroColor = {fg = colorSeven, bold = true}
-theme.typeColor = {fg = colorTen, italic = true, bold = true}
-theme.textColor = {fg = colorThree}
-theme.operatorColor = {fg = colorOne}
-theme.specialTextColor = {fg = colorEight}
-theme.commentColor = {fg = colorFour}
-theme.commentWarningColor = {fg = "#000000", bg = colorSeven}
-theme.commentTodoColor = {fg = "#000000", bg = colorEight}
-theme.commentErrorColor = {fg = "#000000", bg = colorThree}
-theme.commentNoteColor = {fg = "#000000", bg = colorOne}
-theme.characterColor = {fg = colorFour, bold = true, underline = true}
-theme.netrwDirectoryColor = {fg = colorFour, bold = true, underline = true}
-theme.netrwPlainColor = {fg = colorFive}
-theme.symbolColor = {fg = colorNine}
-theme.normalColor = {bg = backgroundOne}
-
-theme.lineNumber = {fg = colorFive, bg=backagroundTwo, bold = true};
-theme.columnLineColor = {bg = backagroundTwo};
-theme.nonText = {fg = colorSeven}
-theme.menuColor = {fg=colorFive}
-theme.cursorColumn = {bg = backgroundOne}
-theme.menuTextColor = {fg = colorOne}
-theme.titleColor = {fg = colorFour}
-theme.searchColor = {bg = colorFour, fg = colorSeven}
-]]
 return theme;
--- lua/crookedpastel/pastellight.lua
+++ lua/crookedpastel/pastellight.lua
@@ -18,41 +18,55 @@ local theme = crooked.NewTheme();
 
 theme.name = "Pastel Light"
 
-theme.keywordColor = {fg = colorTen, bold = true}
-theme.stringColor = {fg = colorTwo, italic = true, underline = true}
-theme.fieldColor = {fg = colorSix, bold = false}
-theme.classColor = {fg = colorTwo, bold = true}
-theme.variableColor = {fg = colorTwo}
-theme.booleanColor = {fg = colorFour, bold = true, underline = true}
-theme.constantColor = {fg = colorSeven, underline = true}
-theme.functionColor = {fg = colorSeven, bold = true}
-theme.builtinFunctionColor = {bold = true, underline = true, italic = true}
-theme.attributeColor = {fg = colorFive}
-theme.conditionalColor = {fg = colorSix}
-theme.moduleColor = {fg = colorEight}
-theme.macroColor = {fg = colorNine, bold = true}
-theme.typeColor = {fg = colorTen, italic = true, bold = true}
-theme.textColor = {fg = colorThree}
-theme.operatorColor = {fg = colorOne}
-theme.specialTextColor = {fg = colorEight}
-theme.commentColor = {fg = colorFour}
-theme.commentWarningColor = {fg = "#000000", bg = colorSeven}
-theme.commentTodoColor = {fg = "#000000", bg = colorEight}
-theme.commentErrorColor = {fg = "#000000", bg = colorThree}
-theme.commentNoteColor = {fg = "#000000", bg = colorOne}
-theme.characterColor = {fg = colorFour, bold = true, underline = true}
-theme.netrwDirectoryColor = {fg = colorFour, bold = true, underline = true}
-theme.netrwPlainColor = {fg = colorFive}
-theme.symbolColor = {fg = colorNine}
-theme.normalColor = {bg = backgroundOne}
 
-theme.lineNumber = {fg = colorFive, bg=backagroundTwo, bold = true};
-theme.columnLineColor = {bg = backagroundTwo};
-theme.nonText = {fg = colorSeven}
-theme.menuColor = {fg=colorFive}
-theme.cursorColumn = {bg = backgroundOne}
-theme.menuTextColor = {fg = colorOne}
-theme.titleColor = {fg = colorFour}
-theme.searchColor = {bg = colorFour, fg = colorSeven}
+theme.base.number = {fg = colorSeven, underline = true}
+theme.base.string = {fg = colorTwo, italic = true, underline = true}
+theme.base.type = {fg = colorSeven, bold = true}
+theme.base.method = {fg = colorThree, bold = true}
+theme.base.identifier = {fg = colorTwo}
+theme.base.statement = {fg = colorFive}
+theme.base.comment = {fg = colorFour, italic = true}
+theme.base.constant = {fg = colorSeven, underline = true}
+theme.base.preproc = {fg = colorTwo, bold = true}
+theme.base.symbol = {fg = colorEight}
+theme.base.boolean = {fg = colorFour, bold = true, underline = true}
+theme.base.keyword = {fg = colorSeven}
+
+theme.editor.lineNumber = {fg = colorFive, bg = backagroundTwo, bold = true}
+theme.editor.colorColumn = {bg = backagroundTwo}
+theme.editor.background = {bg = backgroundOne}
+theme.editor.cursorColumn = {bg = backagroundTwo}
+theme.editor.indentLine = {fg = colorTen}
+theme.editor.indentScope = {fg = colorThree}
+theme.editor.error = {bg = colorFour, fg = "#000000"}
+theme.editor.warning = {fg = colorFive}
+theme.editor.menu = {bg = backagroundTwo}
+theme.editor.title = {fg = colorFive}
+theme.editor.search = {fg = colorFive}
+theme.editor.nontext = {fg = colorFour}
+
+theme.netrw.dir = {fg = colorSeven}
+theme.netrw.exe = {fg = colorFive}
+theme.netrw.symlink = {fg = colorTen}
+theme.netrw.version = {fg = colorFour}
+theme.netrw.gray = {fg = colorOne}
+theme.netrw.date = {fg = colorTen}
+theme.netrw.time = {fg = colorSeven}
+theme.netrw.comment = {fg = colorFour}
+theme.netrw.base = {fg = colorSeven}
+theme.netrw.change = {fg = colorThree}
+theme.netrw.cursorLine = {bg = backagroundTwo}
+
+theme.keyword.modifier = {fg = colorEight}
+theme.keyword.builtinType = {fg = colorSeven, bold = true, italic = true}
+theme.keyword.builtinMethod = {fg = colorSeven, bold = true, italic = true}
+theme.keyword.loop = {fg = colorSeven, italic = true}
+theme.keyword.returning = {fg = colorFive, underline = true, italic = true, bold = true}
+theme.keyword.conditional = {fg = colorSix, bold = true}
+theme.keyword.macroConstant = {fg = colorSeven, underline = true}
+theme.keyword.import = {fg = colorSeven, italic = true}
+theme.keyword.define = {fg  = colorThree, italic = true}
+theme.keyword.builtinConstant = {fg = colorSeven, underline = true, bold = true}
+theme.keyword.definitions = {fg = colorThree, bold = true, italic = true}
 
 return theme;
